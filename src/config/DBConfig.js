2d7631f8cf1ceed7415f17e6a6a55c54a9e920e8
unknown <niteshpatelsjd@gmail.com>
2026-05-25 22:53:49 +0530

    latest code updated

--- src/config/DBConfig.js
+++ src/config/DBConfig.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 module.exports = async function connectDB() {
-  const uri = process.env.MONGO_URL || "mongodb://localhost:27017/render_worx_db?authSource=admin";
+ // const uri = process.env.MONGO_URL || "mongodb://localhost:27017/render_worx_db?authSource=admin";
+  const uri = process.env.MONGO_URL;
   await mongoose.connect(uri);
-  console.log("UserService Mongo connected");
+  console.log("Admin Service Mongo connected");
 };
